6971825d2fc4f20718de8de8fadc8b507163c214
Pedro Barros <pedro.barros@toptal.com>
2024-10-29 09:22:09 -0300

    feat: add commitlint, lint-staged and husky

--- apps/coins/src/app/features/user-coins/user-coins.component.ts
+++ apps/coins/src/app/features/user-coins/user-coins.component.ts
@@ -1,19 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { Store } from '@ngrx/store';
+
 import { UserQueries } from '@angular-nx-firebase-monorepo/firestore/queries';
+import { Store } from '@ngrx/store';
 
 import { UserCoinsActions } from './store/user-coins.actions';
 import { selectAll } from './store/user-coins.reducer';
@@ -29,14 +25,14 @@ import { selectAll } from './store/user-coins.reducer';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatInputModule,
+    MatInputModule
   ],
   templateUrl: './user-coins.component.html',
-  styleUrl: './user-coins.component.scss',
+  styleUrl: './user-coins.component.scss'
 })
 export class UserCoinsComponent implements OnInit {
   form = new FormGroup({
-    coin: new FormControl('', { nonNullable: true }),
+    coin: new FormControl('', { nonNullable: true })
   });
   userQueries = inject(UserQueries);
   auth = inject(Auth);
